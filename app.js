d8c8f78efb33f2e6a5f57fffc13505878700754c
aboli3514 <39972127+aboli3514@users.noreply.github.com>
2021-01-23 14:00:21 -0500

    app.js now sends image files to client

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ app.use(express.static('public'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/img/Asteroid1.png'));
+    res.sendFile(path.join(__dirname + '/img/Asteroid2.png'));
+    res.sendFile(path.join(__dirname + '/img/Background.png'));
+    res.sendFile(path.join(__dirname + '/img/Rocket1.png'));
+    res.sendFile(path.join(__dirname + '/img/Rocket1Boost.png'));
+    res.sendFile(path.join(__dirname + '/img/Satellite1.png'));
+    res.sendFile(path.join(__dirname + '/img/SpacemanJL.png'));
+    res.sendFile(path.join(__dirname + '/img/SpacemanJR.png'));
+    res.sendFile(path.join(__dirname + '/img/SpacemanL.png'));
+    res.sendFile(path.join(__dirname + '/img/SpacemanR.png'));
 })
 
 app.listen(process.env.PORT || 8000);
