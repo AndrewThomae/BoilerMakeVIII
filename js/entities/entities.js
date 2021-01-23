16603b0e1934404cad81458e1af784b54f6ad89d
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2021-01-23 11:07:30 -0500

    Step2 working

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -9,47 +9,12 @@ game.PlayerEntity = me.Entity.extend({
     init:function (x, y, settings) {
         // call the constructor
         this._super(me.Entity, 'init', [x, y , settings]);
-        // max velocities
-    this.body.setMaxVelocity(3, 15);
-    this.body.setFriction(0.4, 0);
-
-    // set the display to follow our position on both axis
-    me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH, 0.4);
-
-    // ensure the player is updated even when outside of the viewport
-    this.alwaysUpdate = true;
-
-    // define a basic jumping animation (using all frames)
-    this.renderable.addAnimation("jump", [0, 1, 2, 3, 4, 5, 6, 7]);
-
-    // define a basic resting animation (using the first frame)
-    this.renderable.addAnimation("rest", [0]);
-
-    // set the resting animation as default
-    this.renderable.setCurrentAnimation("rest");
     },
-
-    
 
     /**
      * update the entity
      */
     update : function (dt) {
-      if (me.input.isKeyPressed('left') && me.input.isKeyPressed('jump')) {
-        // update the default force
-        this.body.force.x = -this.body.maxVel.x;
-        this.body.force.y = this.body.maxVel.y;
-        // change to jumping animation
-        if (!this.renderable.isCurrentAnimation("jump")) {
-          this.renderable.setCurrentAnimation("jump");
-        }
-      } else if (me.input.isKeyPressed('right') && me.input.isKeyPressed('jump')) {
-        // update the default force
-        this.body.force.x = this.body.maxVel.x;
-        this.body.force.y = this.body.maxVel.y;
-      } else {
-        // TODO
-      }
 
         // apply physics to the body (this moves the entity)
         this.body.update(dt);
@@ -70,4 +35,3 @@ game.PlayerEntity = me.Entity.extend({
         return true;
     }
 });
-
