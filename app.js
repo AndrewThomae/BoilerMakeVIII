9f0b016a88280c95ba3e4d0587b08f807229767e
aboli3514 <39972127+aboli3514@users.noreply.github.com>
2021-01-23 13:46:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var path = require('path')
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    res.sendFile('/index.html');
+    res.sendFile(path.join(__dirname + '/index.html'));
 })
 
 app.listen(process.env.PORT || 8000);
