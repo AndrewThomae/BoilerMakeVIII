ba095a9eb584ce3ddb7c1170c6b6f7b2ae8e5d30
Alex-Bolinger <alex.bolinger3514@gmail.com>
2025-02-28 12:29:38 -0500

    modified for easier local server setup

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ var express = require('express');
 var app = express();
 var path = require('path')
 
+app.get('/img/*', function(req, res) {
+    console.log(req.route)
+    res.sendFile(path.join(__dirname, req.path));
+});
+
 app.get('/', function(req, res) {
+    console.log(req.route)
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/img/SpacemanL.png', function(req, res) {
-    res.sendFile(path.join(__dirname));
-});
 
 app.listen(process.env.PORT || 8000);
 
